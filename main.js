063c6905036a036e630e71c88563d2f4ff49b01a
NycolasR <nycolasramon3@gmail.com>
2020-07-03 01:34:06 -0300

    Aula sobre Object Short Syntax

--- main.js
+++ main.js
@@ -1,52 +1,10 @@
-// REST
+const nome = 'Nycolas'
+const idade = 19
 
-const usuario0 = {
-    nome: "Nycolas",
-    idade: 19,
-    universidade: "IFPB"
-}
-
-const { nome, ...resto } = usuario0;
-// ... -> rest operator
-
-//console.log(nome) // receberá o nome do usuário
-//console.log(resto) // receberá o resto das propriedades do usuário
-
-
-//Exemplo com arrays
-const arr = [1, 2, 3, 4]
-
-const [ a, b, ...c ] = arr
-
-//console.log(a)
-//console.log(b)
-//console.log(c)
-
-// Exemplo para parâmetros de entrada de funções
-function soma(a, b, ...parametros) {
-    return a + b + parametros.reduce((total, next) => total + next);
-}
-
-//console.log(soma(1, 3, 4, 6));
-
-
-// SPREAD
-
-// Exemplo de soma entre arrays
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [ ...arr1, ...arr2 ];
-
-//console.log(arr3)
-
-// Exemplo com usuários
-const usuario1 = {
-    nome: "Nycolas",
-    idade: 19,
-    universidade: "IFPB"
+const usuario = {
+    nome, // Se o valor do atributo "nome" for o mesmo da variável "nome", "nome: nome" é o mesmo que "nome,"
+    idade,
+    empresa: 'IFPB',
 };
 
-const usuario2 = { ...usuario1, nome: 'Gabriel' }
-
-console.log(usuario2)
+console.log(usuario)
