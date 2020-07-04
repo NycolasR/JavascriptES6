e8cdee101b912d1391e349e4e479c0c13eea6ff3
NycolasR <nycolasramon3@gmail.com>
2020-07-04 17:45:17 -0300

    Webpack configurado

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-const nome = 'Nycolas'
-const idade = 19
+import { soma } from './funcoes';
 
-const usuario = {
-    nome, // Se o valor do atributo "nome" for o mesmo da variável "nome", "nome: nome" é o mesmo que "nome,"
-    idade,
-    empresa: 'IFPB',
-};
-
-console.log(usuario)
+console.log(soma(1, 2));
