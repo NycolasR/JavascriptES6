a8a1251b4d56dc5ace768d49511236eb7c456331
NycolasR <nycolasramon3@gmail.com>
2020-07-04 18:23:32 -0300

    Webpack-dev-server instalado e configurado

--- main.js
+++
@@ -1,7 +0,0 @@
-//import soma, { sub } from './funcoes'; // O padrão é buscar por um arquivo de extensão .js
-
-import * as funcoes from './funcoes'
-
-console.log(funcoes.soma(1, 2));
-console.log(funcoes.sub(4, 2));
-console.log(funcoes.mult(2, 3));
