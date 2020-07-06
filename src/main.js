5305e5e01eaa7710165d2d41fe035206f64508f4
NycolasR <nycolasramon3@gmail.com>
2020-07-05 22:02:13 -0300

    Resolução da lista de exercícios do módulo 3

--- src/main.js
+++ src/main.js
@@ -1,14 +1,57 @@
-// Respostas - Lista do módulo 2
-// 1.1
-import ClasseUsuario from './functions';
+// Trecho 1
+// Função delay aciona o .then após 1s
+const delay = (time) => new Promise(resolve => setTimeout( () => { resolve(time) } , 1000));
 
-ClasseUsuario.info()
+async function umPorSegundo() {
+    console.log(await delay('1s'));
+    console.log(await delay('2s'));
+    console.log(await delay('3s'));
+}
 
-// 1.2
-import { idade } from './functions';
-console.log(`A idade do usuário é de ${idade} anos`)
+//umPorSegundo()
 
-// 1.3
-import ClasseUsuario2, { idade as idadeUsuario } from './functions';
-ClasseUsuario2.info()
-console.log(idadeUsuario)
+
+// Trecho 2
+import axios from 'axios';
+
+const getUserFromGithub = async (user) => {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user}`)
+        console.log(response.data);
+    } catch(err) {
+        console.log('O usário não existe');
+    }
+}
+/*
+getUserFromGithub('diego3g')
+getUserFromGithub('NycolasR')
+getUserFromGithub('diego32342342g')
+*/
+
+// Trecho 3
+class Github {
+    static async getRepositories(repo) {
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${repo}`)
+            console.log(response)
+        } catch(err) {
+            console.log('O repositório não existe');
+        }
+    }
+}
+
+//Github.getRepositories('NycolasR/CursoJS');
+//Github.getRepositories('rocketseat/sdfsdfsdfs');
+
+// Trecho 4
+const buscaUsuario = async (usuario) => {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${usuario}`)
+        console.log(response);
+    } catch(err) {
+            console.log('O usuario não existe');
+    }
+}
+
+buscaUsuario('NycolasR')
+buscaUsuario('NycolasRdfdfsdfs')
