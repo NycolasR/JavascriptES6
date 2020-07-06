988218e68315ac08e2f8882b662601673e3c2aec
NycolasR <nycolasramon3@gmail.com>
2020-07-06 19:32:05 -0300

    Mensagem de loading e tratamento de erros aplicados à classe App

--- src/main.js
+++ src/main.js
@@ -17,7 +17,19 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-     async addRepository(event) {
+    setLoading(loading = true) {
+        if(loading) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event) {
         // isto vai prevenir que o form tenha o funcionamento
         // comum de recarregar a página, usar o método GET ou POST.
         event.preventDefault();
@@ -28,22 +40,31 @@ class App {
             return;
         }
 
-        const response = await api.get(`/repos/${repoInput}`);
-        
-        const { name, description, html_url, owner: { avatar_url } } = response.data;
+        this.setLoading();
 
-        this.repositories.push({
-            name,
-            description,
-            avatar_url,
-            html_url,
-        });
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
+    
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+    
+            this.render();
+
+        } catch(err) {
+            alert('O repositório não existe!')
+        }
 
         this.inputEl.value = '';
         this.inputEl.focus();
 
-        this.render();
-    }
+        this.setLoading(false);
+    }  
 
     render() {
         this.listEl.innerHTML = ''
